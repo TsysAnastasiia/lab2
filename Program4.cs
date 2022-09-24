683b817c0f46d8575fa35ff3a58448efd1c2061d
TsysAnastasiia <112326472+TsysAnastasiia@users.noreply.github.com>
2022-09-24 21:54:43 +0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+     class Program4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User solution\n");
+
+            User ezUser = new User("QWERTY", "Denches", "Danilenko", 18);
+
+            Console.WriteLine(ezUser.login);
+            Console.WriteLine(ezUser.name);
+            Console.WriteLine(ezUser.surname);
+            Console.WriteLine(ezUser.age);
+            Console.WriteLine(ezUser.date);
+
+            Console.ReadKey();
+        }
+    }
+}
