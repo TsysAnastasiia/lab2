31a6a6b618a87f7a56e05f282d4692365342e3fd
TsysAnastasiia <112326472+TsysAnastasiia@users.noreply.github.com>
2022-09-24 21:44:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address solution\n");
+            Address someAddress = new Address();
+
+            someAddress.Index = 0266;
+            someAddress.Country = "Ukraine";
+            someAddress.City = "Kyiv";
+            someAddress.Street = "Unnamed";
+            someAddress.House = 27;
+            someAddress.Apartment = 8;
+
+            Console.WriteLine(someAddress.GetAddress());
+
+            Console.ReadKey();
+        }
+    }
+}
